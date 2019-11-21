ea677147e74d04f8491aeecddf85340093d2aff3
Haxors Programming Club <44484256+haxorsprogramming@users.noreply.github.com>
2019-11-21 00:48:21 -0800

    Update sintaksumum.js

--- sintaksumum.js
+++ sintaksumum.js
@@ -5,7 +5,7 @@
       $('#divUtama').load('tampilAll.php');
       
    
-    
+    //pake buat tag
       $('#divUtama').on('click','.btnKembali',function(){
        $('#divUtama').html("Memuat form ...");
       $('#divUtama').load('tampilAll.php');
